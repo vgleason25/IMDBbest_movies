cf89d7650d7622a1a26cff6c2c13c4f91006e7b3
Vanessa Gleason <vgleason25@gmail.com>
2022-11-23 16:28:10 -0600

    store data in  SQL option

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -8,6 +8,46 @@
 from itemadapter import ItemAdapter
 import logging 
 import pymongo 
+import sqlite3
+
+class SQLitePipeline(object):
+
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect("imdb.db")
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute('''
+                CREATE TABLE best_movies(
+                    title TEXT,
+                    year TEXT,
+                    duration TEXT,
+                    genre TEXT,
+                    rating TEXT,
+                    imdb_rating TEXT,
+                    movie_url TEXT
+                )
+            ''') 
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def close_spider(self, spider):
+       self.connection.close()
+
+    def process_item(self, item, spider):
+        self.c.execute('''
+            INSERT INTO best_movies (title,year,duration,genre,rating,imdb_rating,movie_url) VALUES(?,?,?,?,?,?,?)
+        ''', (
+            item.get('title'),
+            item.get('year'),
+            item.get('duration'),
+            item.get('genre'),
+            item.get('rating'),
+            item.get('imdb_rating'),
+            item.get('movie_url')
+        ))
+        self.connection.commit()
+        return item
 
 class MongodbPipeline(object):
     collection_name = "best_movies"
--- imdb/settings.py
+++ imdb/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'imdb.pipelines.MongodbPipeline': 300
+   'imdb.pipelines.SQLitePipeline': 300
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
